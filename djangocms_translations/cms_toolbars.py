799458b7860dab6a9c173a35eb85c82fe882a310
albanbochsler <alban.bochsler@gmail.com>
2024-12-10 17:16:02 +0100

    Display translate page toolbar menu only on pages

--- djangocms_translations/cms_toolbars.py
+++ djangocms_translations/cms_toolbars.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.urls import reverse
 from django.utils.translation import get_language_from_request
 from django.utils.translation import gettext_lazy as _
+from django.apps import apps
 
 from cms.toolbar_base import CMSToolbar
 from cms.toolbar_pool import toolbar_pool
@@ -38,11 +39,6 @@ class TranslationsToolbar(CMSToolbar):
             .format(url=reverse('admin:create-translation-request'), page_id=page.pk, source_language=current_language)
         )
 
-        translate_menu = menu.get_or_create_menu(
-            'djangocms_translations-translate',
-            _('Translate this page'),
-            position=1,
-        )
 
         overview_menu = menu.get_or_create_menu(
             'djangocms_translations-overview',
@@ -52,10 +48,15 @@ class TranslationsToolbar(CMSToolbar):
 
         overview_menu.add_modal_item("Pages", url=overview_url)
         overview_menu.add_modal_item("Apps", url=overview_url_app)
-
-        for language_data in all_languages:
-            code = language_data[0]
-            if code != current_language:
-                name = get_language_name(code)
-                sub_url = '{}&target_language={}'.format(base_url, code)
-                translate_menu.add_modal_item(_('to {}'.format(name)), url=sub_url)
+        if self.app_path == "cms":
+            translate_menu = menu.get_or_create_menu(
+                'djangocms_translations-translate',
+                _('Translate this page'),
+                position=1,
+            )
+            for language_data in all_languages:
+                code = language_data[0]
+                if code != current_language:
+                    name = get_language_name(code)
+                    sub_url = '{}&target_language={}'.format(base_url, code)
+                    translate_menu.add_modal_item(_('to {}'.format(name)), url=sub_url)
